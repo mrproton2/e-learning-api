d41d867d2a8d0c9abbd372ddfaddea073d0ef813
abhishek50117 <abhishek50117@gmail.com>
2023-10-04 17:56:10 +0530

    ,

---
+++ e-learning/Models/addInquiry.cs
@@ -0,0 +1,21 @@
+﻿namespace e_learning.Models
+{
+    public class addInquiry
+    {
+        public string addinquiry_pk { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string address { get; set; }
+        public string contact_no { get; set; }
+        public string email_id { get; set; }
+        public string gender { get; set; }
+        public string date_of_birth { get; set; }
+        public string previous_qualification { get; set; }
+        public string school_college_name { get; set; }
+        public string stream_name { get; set; }
+        public string substream_name { get; set; }
+        public string createddate { get; set; }
+        public string createdby { get; set; }
+
+    }
+}
---
+++ e-learning/Models/admissionForm.cs
@@ -0,0 +1,49 @@
+﻿namespace e_learning.Models
+{
+    public class admissionForm
+    {
+        public string admissionform_pk { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string address { get; set; }
+        public string contact_no { get; set; }
+        public string email_id { get; set; }
+        public string gender { get; set; }
+        public string date_of_birth { get; set; }
+        public string previous_qualification { get; set; }
+        public string school_college_name { get; set; }
+
+        public string parents_name { get; set; }
+        public string occupation { get; set; }
+        public string income { get; set; }
+        public string parents_email { get; set; }
+        public string parents_contact { get; set; }
+        public string stream_name { get; set; }
+        public string substream_name { get; set; }
+
+        public string batch_name { get; set; }
+        public string total_fees { get; set; }
+        public string discount { get; set; }
+        public string fees_without_gst { get; set; }
+        public string gst { get; set; }
+        public string fees_with_gst { get; set; }
+        public string payment_way { get; set; }
+
+        public string number_of_installment { get; set; }
+        public string installment_amount { get; set; }
+        public string number_of_installment_amount { get; set; }
+        public string number_of_installment_date { get; set; }
+        public string full_payment_amount { get; set; }
+        public string mode { get; set; }
+        public string online_mode_amount { get; set; }
+
+        public string payment_mode { get; set; }
+        public string offline_mode_amount { get; set; }
+        public string createddate { get; set; }
+        public string createdby { get; set; }
+
+
+
+
+    }
+}
